113ea0c2a03495999d47ba4685f488466ea9cc1e
wozhendefuleZ <wozhendefuleZ@163.com>
2023-11-20 15:15:58 +0800

    处理 useEffect hook 执行两遍

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,16 @@ type Products = {
   isFruit: Boolean;
 };
 
-function App() {
-  const [count, setCount] = useState(0);
+const List = () => {
+  useEffect(() => {
+    console.log('进入');
+  }, []);
   let products: Array<Products> = [
     { title: 'Cabbage', isFruit: false, id: 1 },
     { title: 'Garlic', isFruit: false, id: 2 },
     { title: 'Apple', isFruit: true, id: 3 },
   ];
   const [list, setList] = useState(products);
-
-  useEffect(() => {
-    console.log('进入');
-
-    return () => {
-      console.log('离开');
-    };
-  }, []);
-  // useEffect(() => {
-  //   console.log('进入222', count);
-  //   // return () => {
-  //   //   console.log('离开');
-  //   // };
-  // }, [count]);
-
   const listClick = (e: Products) => {
     setList(
       list.map((v: Products) => {
@@ -41,6 +28,7 @@ function App() {
       })
     );
   };
+
   let productsList = list.map((e: Products) => (
     <div
       key={e.id}
@@ -50,6 +38,13 @@ function App() {
       {e.title}
     </div>
   ));
+
+  return <>{productsList}</>;
+};
+
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div className="flex items-center h-100vh justify-center">
@@ -61,8 +56,7 @@ function App() {
           >
             count is {count}
           </button>
-
-          {productsList}
+          <List></List>
         </div>
       </div>
     </>

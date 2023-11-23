c955dd08229df9bc2cb4703379fb468c03253f53
wozhendefuleZ <wozhendefuleZ@163.com>
2023-11-23 17:58:52 +0800

    组件通讯

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,36 @@ type Products = {
   isFruit: Boolean;
 };
 
-const List = () => {
+type List = {
+  list: Array<Products>;
+  listClick: (e: Products) => void;
+};
+const List = (props: List) => {
+  let productsList = props.list.map((e: Products) => (
+    <div
+      key={e.id}
+      onClick={() => props.listClick(e)}
+      className={[e.isFruit ? 'text-red' : 'text-green', 'mt-10px'].join(' ')}
+    >
+      {e.title}
+    </div>
+  ));
+
+  return <>{productsList}</>;
+};
+
+function App() {
   useEffect(() => {
     console.log('进入');
-  }, []);
+  }, []); // 数组内有变量那么变动是会继续触发
+
   let products: Array<Products> = [
     { title: 'Cabbage', isFruit: false, id: 1 },
     { title: 'Garlic', isFruit: false, id: 2 },
     { title: 'Apple', isFruit: true, id: 3 },
   ];
   const [list, setList] = useState(products);
+
   const listClick = (e: Products) => {
     setList(
       list.map((v: Products) => {
@@ -29,34 +49,11 @@ const List = () => {
     );
   };
 
-  let productsList = list.map((e: Products) => (
-    <div
-      key={e.id}
-      onClick={() => listClick(e)}
-      className={[e.isFruit ? 'text-red' : 'text-green', 'mt-10px'].join(' ')}
-    >
-      {e.title}
-    </div>
-  ));
-
-  return <>{productsList}</>;
-};
-
-function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="flex items-center h-100vh justify-center">
         <div>
-          <button
-            className="text-red p-5px"
-            style={{ width: '100px' }}
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is {count}
-          </button>
-          <List></List>
+          <List list={list} listClick={listClick}></List>
         </div>
       </div>
     </>

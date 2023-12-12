61a0336de998755e3a44d42f57027bc5b9b06ea3
wozhendefuleZ <wozhendefuleZ@163.com>
2023-12-12 14:57:49 +0800

    预览组件新增滚动放大缩小

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -3,7 +3,7 @@ import {
   RightCircleOutlined,
   CloseCircleOutlined,
 } from '@ant-design/icons';
-import { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import './index.css';
 
@@ -11,6 +11,7 @@ const Preview = (props: any) => {
   const imgUrl = useMemo(() => {
     return props.images[props.index];
   }, [props.index]);
+  const [scale, setScale] = useState(1); // 初始缩放比例为1
   const changeImage = (type: string) => {
     const fn: { [key: string]: () => any } = {
       prev: (): void => {
@@ -35,6 +36,27 @@ const Preview = (props: any) => {
     fn[type]();
   };
 
+  const handleWheel = (event: React.WheelEvent<HTMLImageElement>) => {
+    event.preventDefault();
+
+    // 滚轮向上滚动，放大图片
+    if (event.deltaY < 0) {
+      setScale((prevScale) => prevScale * 1.1); // 可以根据需要调整缩放系数
+    }
+    // 滚轮向下滚动，缩小图片
+    else {
+      setScale((prevScale) => prevScale / 1.1); // 可以根据需要调整缩放系数
+    }
+
+    // 限制最小和最大缩放比例，以防图片过大或过小
+    if (scale < 0.1) {
+      setScale(0.1);
+    }
+    if (scale > 3) {
+      setScale(3);
+    }
+  };
+
   return (
     <div>
       <div className="fixed top-0 left-0 right-0 bottom-0 bg-black/50"></div>
@@ -56,9 +78,11 @@ const Preview = (props: any) => {
 
       <div className="preview-container">
         <img
+          style={{ transform: `scale(${scale})` }}
           src={imgUrl}
           className="max-h-100vh max-w-100vw select-none"
           alt="Preview Image"
+          onWheel={handleWheel} // 监听滚轮事件
         />
       </div>
     </div>
